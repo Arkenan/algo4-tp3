7fa97a3e8dc0c6c5c7068e786c74c30560a3cb28
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2021-02-12 15:45:05 -0300

    Intengo de agregar DB de nuevo

--- src/main/scala/edu/fiuba/fpfiuba43/Main.scala
+++ src/main/scala/edu/fiuba/fpfiuba43/Main.scala
@@ -1,13 +1,15 @@
 package edu.fiuba.fpfiuba43
 
 import cats.effect.{ContextShift, ExitCode, IO, IOApp}
-import edu.fiuba.fpfiuba43.http.Fpfiuba43Server
+import edu.fiuba.fpfiuba43.http.{DB, Fpfiuba43Server}
 
 import scala.concurrent.ExecutionContext
 
 object Main extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
+
+    val db = DB()
     Fpfiuba43Server.stream[IO].compile.drain.as(ExitCode.Success)
   }
 }
