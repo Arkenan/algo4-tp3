0fbd1ccb641f36ddb112ef2ec8871cfa31b4b9c6
luciakasman <lucia.kasman@gmail.com>
2021-02-12 19:40:34 -0300

    add hashcode and structure the service

--- src/main/scala/edu/fiuba/fpfiuba43/services/ScoreService.scala
+++ src/main/scala/edu/fiuba/fpfiuba43/services/ScoreService.scala
@@ -7,6 +7,7 @@ import org.http4s.EntityDecoder
 import org.http4s.circe._
 import org.http4s.client.Client
 import doobie.implicits._
+import edu.fiuba.fpfiuba43.Scorer
 
 trait ScoreService[F[_]] {
   def getScore(row: InputRow): F[Score]
@@ -17,8 +18,25 @@ class ScoreServiceRest[F[_]: Sync:Effect](client: Client[F], tr: Resource[F, Hik
   implicit val decoder: EntityDecoder[F, Score] = jsonOf[F, Score]
 
   override def getScore(row: InputRow): F[Score] = {
-    tr.use { xa =>
-       sql"select score from fptp.scores where hash_code= ${row.id}".query[Score].unique.transact(xa)
+    val hashedRow = row.hashCode()
+    val result = getValueFromCache(hashedRow)
+
+    result match {
+      case null => {
+        val score = Scorer.score(row)
+        saveScoreInCache(score)
       }
+      case _ => result
     }
   }
+
+  def getValueFromCache(hashedRow: Double): F[Option[Score]] = {
+    tr.use { xa =>
+      sql"select score from fptp.scores where hash_code= ${hashedRow}".query[Score].option.transact(xa)
+    }
+  }
+
+  def saveScoreInCache(score: Score): Unit = {
+
+  }
+}
